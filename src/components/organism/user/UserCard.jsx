a1f07d8f79558f1e9799dd4afc3c551bf6f7cc02
USERNAME <shinozakiyume88@gmail.com>
2023-03-22 18:57:21 +0900

    Organismの作成1

---
+++ src/components/organism/user/UserCard.jsx
@@ -0,0 +1,19 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img alt="プロフィール" />
+      <p>名前</p>
+      <dl>
+        <dt>メール</dt>
+        <dd>11111@aaa.com</dd>
+        <dt>TEL</dt>
+        <dd>000-9999-8888</dd>
+        <dt>会社名</dt>
+        <dd>ああああああああああああ</dd>
+        <dt>WEB</dt>
+        <dd>htttttttttt.com</dd>
+      </dl>
+    </div>
+  );
+};
+
