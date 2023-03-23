db83a2caf5be67c8ffa630e5523af9af05effdc5
USERNAME <shinozakiyume88@gmail.com>
2023-03-23 22:43:21 +0900

    Pageの作成

--- src/App.js
+++ src/App.js
@@ -1,28 +1,6 @@
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButon } from "./components/atoms/button/SecondaryButon";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organism/user/UserCard";
+import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  name: "じゃけぇ",
-  image: "https://source.unsplash.com/photos/NEOXGVKTmsA",
-  email: "12345@example.com",
-  phone: "090-1111-2222",
-  company: {
-    name: "テスト会社"
-  },
-  website: "https://google.com"
-};
-
 export default function App() {
-  return (
-    <div className="App">
-      <PrimaryButton>テスト</PrimaryButton>
-      <SecondaryButon>検索</SecondaryButon>
-      <br />
-      <SearchInput />
-      <UserCard user={user} />
-    </div>
-  );
+  return <Router />;
 }
---
+++ src/components/pages/Users.jsx
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organism/user/UserCard";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `じゃけぇ${val}`,
+    image: "https://source.unsplash.com/photos/NEOXGVKTmsA",
+    email: "12345@example.com",
+    phone: "090-1111-2222",
+    company: {
+      name: "テスト会社"
+    },
+    website: "https://google.com"
+  };
+});
+
+export const Users = () => {
+  return (
+    <SContainer>
+      <h2>ユーザー一覧</h2>
+      <SearchInput />
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </SUserArea>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  display: flex;
+  text-align: center;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
+`;
