80f8cbd367d9ead57623371bed01a96189da41e5
USERNAME <shinozakiyume88@gmail.com>
2023-03-23 10:49:48 +0900

    Organismの作成2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButon } from "./components/atoms/button/SecondaryButon";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organism/user/UserCard";
 import "./styles.css";
+
+const user = {
+  name: "じゃけぇ",
+  image: "https://source.unsplash.com/photos/NEOXGVKTmsA",
+  email: "12345@example.com",
+  phone: "090-1111-2222",
+  company: {
+    name: "テスト会社"
+  },
+  website: "https://google.com"
+};
 
 export default function App() {
   return (
@@ -10,6 +22,7 @@ export default function App() {
       <SecondaryButon>検索</SecondaryButon>
       <br />
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
--- src/components/organism/user/UserCard.jsx
+++ src/components/organism/user/UserCard.jsx
@@ -1,19 +1,34 @@
-export const UserCard = () => {
+import styled from "styled-components";
+import { Card } from "../../atoms/card/Card";
+import { UserIconWithName } from "../../molecules/user/UserIconWithName";
+
+export const UserCard = (props) => {
+  const { user } = props;
   return (
-    <div>
-      <img alt="プロフィール" />
-      <p>名前</p>
-      <dl>
+    <Card>
+      <UserIconWithName image={user.image} name={user.name} />
+      <SDl>
         <dt>メール</dt>
-        <dd>11111@aaa.com</dd>
+        <dd>{user.email}</dd>
         <dt>TEL</dt>
-        <dd>000-9999-8888</dd>
+        <dd>{user.phone}</dd>
         <dt>会社名</dt>
-        <dd>ああああああああああああ</dd>
+        <dd>{user.company.name}</dd>
         <dt>WEB</dt>
-        <dd>htttttttttt.com</dd>
-      </dl>
-    </div>
+        <dd>{user.website}</dd>
+      </SDl>
+    </Card>
   );
 };
 
+const SDl = styled.dl`
+  text-align: left;
+  dt {
+    float: left;
+  }
+  dd {
+    padding-left: 32px;
+    padding-bottom: 8px;
+    overflow-wrap: break-word;
+  }
+`;
