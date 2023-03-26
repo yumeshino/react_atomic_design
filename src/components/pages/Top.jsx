c87fe3cc713c745d8b827ab85ad94beae2f7fc06
USERNAME <shinozakiyume88@gmail.com>
2023-03-26 19:17:25 +0900

    ツラい例を実装してみる

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,9 +1,22 @@
+import React from "react";
 import styled from "styled-components";
+import { useHistory } from "react-router-dom";
+import { SecondaryButon } from "../atoms/button/SecondaryButon";
 
 export const Top = () => {
+  const history = useHistory();
+  const oncliCkAdmin = () =>
+    history.push({ pathname: "/users", state: { isAdmin: true } });
+  const oncliCkGeneral = () =>
+    history.push({ pathname: "/users", state: { isAdmin: false } });
+
   return (
     <SContainer>
       <h2>TOPページです</h2>
+      <SecondaryButon onClick={oncliCkAdmin}>管理者ユーザー</SecondaryButon>
+      <br />
+      <br />
+      <SecondaryButon onClick={oncliCkGeneral}>一般ユーザー</SecondaryButon>
     </SContainer>
   );
 };
